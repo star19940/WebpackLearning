022193f4edf727c1ee67d50f496612bc782525af
star <1453872893@qq.com>
2020-08-24 15:28:46 +0800

    feat:管理输出、开发环境

---
+++ webpack-demo2/src/index.js
@@ -0,0 +1,19 @@
+import _ from 'lodash';
+import printMe from './print.js';
+
+function component() {
+    const element = document.createElement('div');
+    const btn = document.createElement('button');
+
+    // lodash（目前通过一个 script 引入）对于执行这一行是必需的
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    btn.innerHTML = '点击这里，然后查看 console！';
+    btn.onclick = printMe;
+ 
+    element.appendChild(btn);
+
+    return element;
+  }
+  
+document.body.appendChild(component());
